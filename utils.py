6e3ee2b4ab63b00cbbad969ae406af07e976dd4d
Patrick <81873830+MadPatrick@users.noreply.github.com>
2026-03-09 20:59:58 +0100

    Fixes in code
    filter_devices now returns [] instead of None, replaced inefficient json.dumps check with any(), fixed operator precedence issue with explicit brackets, removed duplicate handle_response.

--- utils.py
+++ utils.py
@@ -2,98 +2,111 @@ import logging
 import exceptions
 import json
 
-stateSet = {"core:ClosureState","core:OpenClosedState","core:LuminanceState","core:DeploymentState"} #list of states that trigger domoticz widget update
+# States that trigger a Domoticz widget update
+stateSet = {
+    "core:ClosureState",
+    "core:OpenClosedState",
+    "core:LuminanceState",
+    "core:DeploymentState"
+}
+
+# Supported uiClasses for io:// and rts:// devices
+_SUPPORTED_IO_CLASSES = {
+    "RollerShutter",
+    "LightSensor",
+    "ExteriorScreen",
+    "Screen",
+    "Awning",
+    "Pergola",
+    "GarageDoor",
+    "Gate",
+    "Window",
+    "VenetianBlind",
+    "ExteriorVenetianBlind"
+}
 
 def filter_devices(Data):
     logging.debug("start filter devices")
 
-    if (not "uiClass" in json.dumps(Data)):
+    # OPT 6: retourneer lege lijst in plaats van None zodat callers
+    # veilig len() / iteratie kunnen gebruiken zonder TypeError.
+    # OPT 6b: efficiÃ«nte check via any() i.p.v. json.dumps van de volledige dataset
+    if not any("uiClass" in str(d.get("definition", {})) for d in Data):
         logging.error("filter_devices: missing uiClass in response")
         logging.debug(str(Data))
-        return
+        return []
 
-    filtered_devices = list()
+    filtered_devices = []
     for device in Data:
-        logging.debug("filter_devices: Device name: "+device["label"]+" Device class: "+device["definition"]["uiClass"])
-        if (((device["definition"]["uiClass"] == "RollerShutter") 
-            or (device["definition"]["uiClass"] == "LightSensor") 
-            or (device["definition"]["uiClass"] == "ExteriorScreen") 
-            or (device["definition"]["uiClass"] == "Screen") 
-            or (device["definition"]["uiClass"] == "Awning") 
-            or (device["definition"]["uiClass"] == "Pergola") 
-            or (device["definition"]["uiClass"] == "GarageDoor")
-            or (device["definition"]["uiClass"] == "Gate")        
-            or (device["definition"]["uiClass"] == "Window") 
-            or (device["definition"]["uiClass"] == "VenetianBlind") 
-            or (device["definition"]["uiClass"] == "ExteriorVenetianBlind")) 
-            and ((device["deviceURL"].startswith("io://")) or (device["deviceURL"].startswith("rts://"))) 
-            or (device["definition"]["uiClass"] == "Pod" and device["deviceURL"].startswith("internal://"))):
+        ui_class  = device["definition"]["uiClass"]
+        device_url = device["deviceURL"]
+        logging.debug("filter_devices: Device name: " + device["label"] + " Device class: " + ui_class)
+
+        # OPT 7: expliciete haakjes toegevoegd zodat de operator-prioriteit van
+        # 'and'/'or' geen onverwachte filter-resultaten geeft
+        is_io_or_rts = device_url.startswith("io://") or device_url.startswith("rts://")
+        is_supported_io = ui_class in _SUPPORTED_IO_CLASSES and is_io_or_rts
+        is_pod = ui_class == "Pod" and device_url.startswith("internal://")
+
+        if is_supported_io or is_pod:
             filtered_devices.append(device)
-            logging.debug("supported device found: "+ str(device))
+            logging.debug("supported device found: " + str(device))
         else:
-            logging.debug("unsupported device found: "+ str(device))
+            logging.debug("unsupported device found: " + str(device))
 
     logging.debug("finished filter devices")
     return filtered_devices
 
+
 def filter_events(Data):
-    """filters relevant (DeviceStateChangedEvent) out of a events list"""
+    """Filters relevant (DeviceStateChangedEvent / DeviceState) events out of an events list."""
     logging.debug("start filter events")
-    filtered_events = list()
+    filtered_events = []
 
     for event in Data:
-        if (event["name"] == "DeviceStateChangedEvent" or event["name"] == "DeviceState"):
-            logging.debug("get_events: add event: URL: '"+event["deviceURL"]+"' num states: '"+str(len(event["deviceStates"]))+"'")
+        if event["name"] in ("DeviceStateChangedEvent", "DeviceState"):
+            logging.debug(
+                "get_events: add event: URL: '" + event["deviceURL"] +
+                "' num states: '" + str(len(event["deviceStates"])) + "'"
+            )
             for event_state in event["deviceStates"]:
-                logging.debug("   get_events: eventname: '"+event_state["name"]+"' with value: '"+str(event_state["value"])+"'")
+                logging.debug(
+                    "   get_events: eventname: '" + event_state["name"] +
+                    "' with value: '" + str(event_state["value"]) + "'"
+                )
             filtered_events.append(event)
 
     logging.debug("finished filter events")
     return filtered_events
 
+
 def filter_states(Data):
-    """filters relevant state data from a device setup API reply"""
+    """Filters relevant state data from a device setup API reply."""
     logging.debug("start filter states")
-    filtered_states = list()
-    deviceURL = ""
-    deviceClass = ""
+    filtered_states = []
 
     for device in Data:
-        stateList = list()
-        deviceURL = device["deviceURL"]
-        deviceClass = device["definition"]["uiClass"]
-        if not "states" in device:
+        device_url   = device["deviceURL"]
+        device_class = device["definition"]["uiClass"]
+
+        if "states" not in device:
             continue
-        for state in device["states"]:
-            if state["name"] in stateSet:
-                stateList.append(state)
-        if len(stateList)>0:
-            stateToAdd = {"deviceURL":deviceURL, 
-                "deviceStates":stateList,
-                "deviceClass":deviceClass,
-                "name":"DeviceState"}
-            filtered_states.append(stateToAdd)
-        logging.debug("Device state: "+str(filtered_states))
+
+        state_list = [
+            state for state in device["states"]
+            if state["name"] in stateSet
+        ]
+
+        if state_list:
+            filtered_states.append({
+                "deviceURL":    device_url,
+                "deviceStates": state_list,
+                "deviceClass":  device_class,
+                "name":         "DeviceState"
+            })
+            logging.debug("Device state: " + str(filtered_states[-1]))
+
     return filtered_states
 
-def handle_response(response, action):
-    """handle faulty responses"""
-    if response.status_code >= 300 and response.status_code < 400:
-        logging.error("status code " + str(response.status_code) + " this is likely a bug")
-        raise exceptions.TahomaException("failed request during "+ action + ": " + str(response.status_code))
-    elif response.status_code == 400:
-        logging.error("status code " + str(response.status_code) + " this is a bug, bad request made, url or body needs to be checked")
-        raise exceptions.TahomaException("failed request during "+ action + ", check url or body: " + str(response.status_code))
-    elif response.status_code == 401:
-        logging.error("status code " + str(response.status_code) + " authorisation failed, check credentials")
-        raise exceptions.TahomaException("failed request during "+ action + ", check credentials: " + str(response.status_code))
-    elif response.status_code == 404:
-        logging.error("status code " + str(response.status_code) + " server not found")
-        raise exceptions.TahomaException("failed request during "+ action + ", server not found: " + str(response.status_code))
-    elif response.status_code >= 500:
-        logging.error("status code " + str(response.status_code) + " a server sided problem")
-        raise exceptions.TahomaException("failed request during "+ action + ": " + str(response.status_code))
-    else:
-        logging.error("status code " + str(response.status_code))
-        raise exceptions.TahomaException("failed request during "+ action + ": " + str(response.status_code))        
-    return
+# OPT 8: handle_response verwijderd â€” was een duplicaat van de methode in
+# tahoma.py en werd nergens aangeroepen vanuit utils.py
